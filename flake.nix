8451883f754edc49f6379cb6e19f5001c6fb2b4d
Massimo Gengarelli <massimo.gengarelli@proton.me>
2024-06-17 08:58:31 +0200

    build: fix GitHub pipeline

--- flake.nix
+++ flake.nix
@@ -24,6 +24,7 @@
           in
           builtins.filterSource (path: _: ! builtins.elem (baseNameOf path) noSrcs) ./.;
 
+        CGO_ENABLED = "0";
         ldflags = [ "-X 'main.Version=${version}'" ];
         vendorHash = "sha256-H79018dCud68fYT0l3IGZXQvD22byhnw/GchsiYJc68=";
       };
